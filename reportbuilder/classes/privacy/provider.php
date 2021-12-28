e8ba7881aae2adeec7a8bfca73a55d6370f854d4
David Matamoros <davidmc@moodle.com>
2021-12-28 12:57:52 +0100

    MDL-72756 reportbuilder: Implement privacy provider

--- reportbuilder/classes/privacy/provider.php
+++ reportbuilder/classes/privacy/provider.php
@@ -18,8 +18,15 @@ declare(strict_types=1);
 
 namespace core_reportbuilder\privacy;
 
+use context;
 use core_privacy\local\metadata\collection;
+use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\core_userlist_provider;
+use core_privacy\local\request\transform;
 use core_privacy\local\request\writer;
+use core_privacy\local\request\approved_userlist;
+use core_privacy\local\request\userlist;
 use core_reportbuilder\local\helpers\user_filter_manager;
 use core_reportbuilder\local\models\audience;
 use core_reportbuilder\local\models\column;
@@ -36,7 +43,9 @@ use core_reportbuilder\local\models\schedule;
  */
 class provider implements
     \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\user_preference_provider {
+    \core_privacy\local\request\user_preference_provider,
+    \core_privacy\local\request\plugin\provider,
+    core_userlist_provider {
 
     /**
      * Returns metadata about the component
@@ -47,8 +56,14 @@ class provider implements
     public static function get_metadata(collection $collection): collection {
         $collection->add_database_table(report::TABLE, [
             'name' => 'privacy:metadata:report:name',
+            'source' => 'privacy:metadata:report:source',
+            'component' => 'privacy:metadata:report:component',
+            'area' => 'privacy:metadata:report:area',
+            'itemid' => 'privacy:metadata:report:itemid',
             'usercreated' => 'privacy:metadata:report:usercreated',
             'usermodified' => 'privacy:metadata:report:usermodified',
+            'timecreated' => 'privacy:metadata:report:timecreated',
+            'timemodified' => 'privacy:metadata:report:timemodified',
         ], 'privacy:metadata:report');
 
         $collection->add_database_table(column::TABLE, [
@@ -65,15 +80,28 @@ class provider implements
 
         $collection->add_database_table(audience::TABLE, [
             'classname' => 'privacy:metadata:audience:classname',
+            'configdata' => 'privacy:metadata:audience:configdata',
             'usercreated' => 'privacy:metadata:audience:usercreated',
             'usermodified' => 'privacy:metadata:audience:usermodified',
+            'timecreated' => 'privacy:metadata:audience:timecreated',
+            'timemodified' => 'privacy:metadata:audience:timemodified',
         ], 'privacy:metadata:audience');
 
         $collection->add_database_table(schedule::TABLE, [
             'name' => 'privacy:metadata:schedule:name',
+            'subject' => 'privacy:metadata:schedule:subject',
+            'message' => 'privacy:metadata:schedule:message',
+            'enabled' => 'privacy:metadata:schedule:enabled',
+            'audiences' => 'privacy:metadata:schedule:audiences',
+            'timescheduled' => 'privacy:metadata:schedule:timescheduled',
+            'timelastsent' => 'privacy:metadata:schedule:timelastsent',
+            'recurrence' => 'privacy:metadata:schedule:recurrence',
+            'timenextsend' => 'privacy:metadata:schedule:timenextsend',
             'userviewas' => 'privacy:metadata:schedule:userviewas',
             'usercreated' => 'privacy:metadata:schedule:usercreated',
             'usermodified' => 'privacy:metadata:schedule:usermodified',
+            'timecreated' => 'privacy:metadata:schedule:timecreated',
+            'timemodified' => 'privacy:metadata:schedule:timemodified',
         ], 'privacy:metadata:schedule');
 
         $collection->add_user_preference('core_reportbuilder', 'privacy:metadata:preference:reportfilter');
@@ -97,5 +125,222 @@ class provider implements
                 $preferencestring
             );
         }
+    }
+
+    /**
+     * Get context export path for a report
+     *
+     * @param report $report
+     * @return array
+     */
+    public static function get_export_path(report $report): array {
+        $reportnode = implode('-', [
+            $report->get('id'),
+            clean_filename($report->get('name')),
+        ]);
+
+        return [get_string('reportbuilder', 'core_reportbuilder'), $reportnode];
+    }
+
+    /**
+     * Get the list of contexts that contain user information for the specified user
+     *
+     * @param int $userid
+     * @return contextlist
+     */
+    public static function get_contexts_for_userid(int $userid): contextlist {
+        $contextlist = new contextlist();
+
+        $select = 'usercreated = ? OR usermodified = ?';
+        $params = [$userid, $userid];
+
+        // We add the system context if the specified user has created/modified any reports or schedules.
+        if (report::record_exists_select($select . ' AND type = 0', $params) ||
+            audience::record_exists_select($select, $params) ||
+            schedule::record_exists_select($select, $params)) {
+
+            $contextlist->add_system_context();
+        }
+
+        return $contextlist;
+    }
+
+    /**
+     * Export all user data for the specified user in the specified contexts
+     *
+     * @param approved_contextlist $contextlist
+     */
+    public static function export_user_data(approved_contextlist $contextlist): void {
+        if (empty($contextlist->count())) {
+            return;
+        }
+
+        $user = $contextlist->get_user();
+
+        // We need to get all reports that the user has created, or reports they have created audience/schedules for.
+        $select = 'type = 0 AND (usercreated = ? OR usermodified = ? OR id IN (
+            SELECT a.reportid
+              FROM {' . audience::TABLE . '} a
+             WHERE a.usercreated = ? OR a.usermodified = ?
+             UNION
+            SELECT s.reportid
+              FROM {' . schedule::TABLE . '} s
+             WHERE s.usercreated = ? OR s.usermodified = ?
+        ))';
+        $params = array_fill(0, 6, $user->id);
+
+        foreach (report::get_records_select($select, $params) as $report) {
+            $contextpath = static::get_export_path($report);
+
+            self::export_report($report);
+
+            $select = 'reportid = ? AND (usercreated = ? OR usermodified = ?)';
+            $params = [$report->get('id'), $user->id, $user->id];
+
+            // Audiences.
+            if ($audiences = audience::get_records_select($select, $params)) {
+                static::export_audiences($report->get_context(), $contextpath, $audiences);
+            }
+
+            // Schedules.
+            if ($schedules = schedule::get_records_select($select, $params)) {
+                static::export_schedules($report->get_context(), $contextpath, $schedules);
+            }
+        }
+    }
+
+    /**
+     * Delete data for all users in context
+     *
+     * @param context $context
+     */
+    public static function delete_data_for_all_users_in_context(context $context): void {
+        // We don't perform any deletion of user data.
+    }
+
+    /**
+     * Delete data for user
+     *
+     * @param approved_contextlist $contextlist
+     */
+    public static function delete_data_for_user(approved_contextlist $contextlist): void {
+        // We don't perform any deletion of user data.
+    }
+
+    /**
+     * Delete data for users
+     *
+     * @param approved_userlist $userlist
+     */
+    public static function delete_data_for_users(approved_userlist $userlist): void {
+        // We don't perform any deletion of user data.
+    }
+
+    /**
+     * Get users in context
+     *
+     * @param userlist $userlist
+     */
+    public static function get_users_in_context(userlist $userlist): void {
+        // Users who have created reports.
+        $sql = 'SELECT usercreated, usermodified
+                  FROM {' . report::TABLE . '}
+                 WHERE type = ?';
+        $userlist->add_from_sql('usercreated', $sql, [0]);
+        $userlist->add_from_sql('usermodified', $sql, [0]);
+
+        // Users who have created audiences.
+        $sql = 'SELECT usercreated, usermodified
+                  FROM {' .  audience::TABLE . '}';
+        $userlist->add_from_sql('usercreated', $sql, []);
+        $userlist->add_from_sql('usermodified', $sql, []);
+
+        // Users who have created schedules.
+        $sql = 'SELECT usercreated, usermodified
+                  FROM {' . schedule::TABLE . '}';
+        $userlist->add_from_sql('usercreated', $sql, []);
+        $userlist->add_from_sql('usermodified', $sql, []);
+    }
+
+    /**
+     * Export given report in context
+     *
+     * @param report $report
+     */
+    protected static function export_report(report $report): void {
+        $reportdata = (object) [
+            'source' => $report->get('source'),
+            'name' => $report->get_formatted_name(),
+            'component' => $report->get('component'),
+            'area' => $report->get('area'),
+            'itemid' => $report->get('itemid'),
+            'conditiondata' => $report->get('conditiondata'),
+            'settingsdata' => $report->get('settingsdata'),
+            'usercreated' => transform::user($report->get('usercreated')),
+            'usermodified' => transform::user($report->get('usermodified')),
+            'timecreated' => transform::datetime($report->get('timecreated')),
+            'timemodified' => transform::datetime($report->get('timemodified')),
+        ];
+
+        $contextpath = self::get_export_path($report);
+        writer::with_context($report->get_context())->export_data($contextpath, $reportdata);
+    }
+
+    /**
+     * Export given audiences in context
+     *
+     * @param context $context
+     * @param array $contextpath
+     * @param audience[] $audiences
+     */
+    protected static function export_audiences(context $context, array $contextpath, array $audiences): void {
+        $audiencedata = [];
+
+        foreach ($audiences as $audience) {
+            $audiencedata[] = (object) [
+                'classname' => $audience->get('classname'),
+                'configdata' => $audience->get('configdata'),
+                'heading' => $audience->get_formatted_heading(),
+                'usercreated' => transform::user($audience->get('usercreated')),
+                'usermodified' => transform::user($audience->get('usermodified')),
+                'timecreated' => transform::datetime($audience->get('timecreated')),
+                'timemodified' => transform::datetime($audience->get('timemodified')),
+            ];
+        }
+
+        writer::with_context($context)->export_related_data($contextpath, 'audiences', (object) ['data' => $audiencedata]);
+    }
+
+    /**
+     * Export given schedules in context
+     *
+     * @param context $context
+     * @param array $contextpath
+     * @param schedule[] $schedules
+     */
+    protected static function export_schedules(context $context, array $contextpath, array $schedules): void {
+        $scheduledata = [];
+
+        foreach ($schedules as $schedule) {
+            $scheduledata[] = (object) [
+                'name' => $schedule->get_formatted_name(),
+                'timescheduled' => transform::datetime($schedule->get('timescheduled')),
+                'recurrence' => $schedule->get('recurrence'),
+                'timelastsent' => transform::datetime($schedule->get('timelastsent')),
+                'timenextsend' => transform::datetime($schedule->get('timenextsend')),
+                'format' => $schedule->get('format'),
+                'enabled' => $schedule->get('enabled'),
+                'audiences' => $schedule->get('audiences'),
+                'userviewas' => $schedule->get('userviewas'),
+                'subject' => format_string($schedule->get('subject')),
+                'message' => format_text($schedule->get('message'), FORMAT_HTML, ['context' => $context]),
+                'usercreated' => transform::user($schedule->get('usercreated')),
+                'usermodified' => transform::user($schedule->get('usermodified')),
+                'timecreated' => transform::datetime($schedule->get('timecreated')),
+                'timemodified' => transform::datetime($schedule->get('timemodified')),
+            ];
+        }
+
+        writer::with_context($context)->export_related_data($contextpath, 'schedules', (object) ['data' => $scheduledata]);
     }
 }
--- reportbuilder/tests/privacy/provider_test.php
+++ reportbuilder/tests/privacy/provider_test.php
@@ -24,6 +24,7 @@ use core_privacy\local\metadata\types\database_table;
 use core_privacy\local\metadata\types\user_preference;
 use core_privacy\local\request\writer;
 use core_privacy\tests\provider_testcase;
+use core_reportbuilder\local\report\base;
 use core_reportbuilder\manager;
 use core_reportbuilder\local\helpers\user_filter_manager;
 use core_reportbuilder\local\models\audience;
@@ -31,6 +32,8 @@ use core_reportbuilder\local\models\column;
 use core_reportbuilder\local\models\filter;
 use core_reportbuilder\local\models\report;
 use core_reportbuilder\local\models\schedule;
+use core_reportbuilder_generator;
+use core_user\reportbuilder\datasource\users;
 
 /**
  * Unit tests for privacy provider
@@ -126,5 +129,181 @@ class provider_test extends provider_testcase {
         // Check that exported user preferences for report2 are correct.
         $report2key = 'reportbuilder-report-' . $report2->get('id');
         $this->assertEquals(json_encode($filtervalues2, JSON_PRETTY_PRINT), $prefs->$report2key->value);
+    }
+
+    /**
+     * Test class export_user_data method
+     */
+    public function test_export_user_data(): void {
+        $this->resetAfterTest();
+
+        /** @var core_reportbuilder_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('core_reportbuilder');
+
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        $context = context_system::instance();
+        $this->export_context_data_for_user((int)$user->id, $context, 'core_reportbuilder');
+
+        $writer = writer::with_context($context);
+        $this->assertFalse($writer->has_any_data());
+
+        $report = $generator->create_report([
+            'name' => 'My report',
+            'type' => base::TYPE_CUSTOM_REPORT,
+            'source' => users::class,
+        ]);
+        $audience = $generator->create_audience([
+            'reportid' => $report->get('id'),
+            'configdata' => [],
+        ]);
+        $schedule = $generator->create_schedule([
+            'reportid' => $report->get('id'),
+            'name' => 'My schedule',
+            'audiences' => json_encode([$audience->get_persistent()->get('id')]),
+            'timescheduled' => strtotime('7 September 2007 08:00'),
+            'recurrence' => schedule::RECURRENCE_DAILY,
+            'timelastsent' => strtotime('28 September 2010 08:00'),
+            'format' => 'pdf',
+            'subject' => 'Hello',
+            'message' => 'Here\'s your weekly report',
+            'enabled' => 1,
+        ]);
+
+        $this->setUser(null);
+
+        $this->export_context_data_for_user((int)$user->id, $context, 'core_reportbuilder');
+
+        $writer = writer::with_context($context);
+        $this->assertTrue($writer->has_any_data());
+
+        $contextpath = provider::get_export_path($report);
+
+        $reportdata = $writer->get_data($contextpath);
+        $this->assertEquals($report->get('name'), $reportdata->name);
+        $this->assertEquals($report->get('source'), $reportdata->source);
+        $this->assertEquals($user->id, $reportdata->usercreated);
+        $this->assertEquals($user->id, $reportdata->usermodified);
+        $this->assertNotEmpty($reportdata->timecreated);
+        $this->assertNotEmpty($reportdata->timemodified);
+
+        // Get audience exported data.
+        $audiencesdata = $writer->get_related_data($contextpath, 'audiences')->data;
+        $this->assertCount(1, $audiencesdata);
+        $this->assertEquals($audience->get_persistent()->get('classname'), $audiencesdata[0]->classname);
+        $this->assertEquals($audience->get_persistent()->get('configdata'), $audiencesdata[0]->configdata);
+        $this->assertEquals($user->id, $audiencesdata[0]->usercreated);
+        $this->assertEquals($user->id, $audiencesdata[0]->usermodified);
+        $this->assertNotEmpty($audiencesdata[0]->timecreated);
+        $this->assertNotEmpty($audiencesdata[0]->timemodified);
+
+        // Get schedules exported data.
+        $schedulesdata = $writer->get_related_data($contextpath, 'schedules')->data;
+        $this->assertCount(1, $schedulesdata);
+        $this->assertEquals($schedule->get('name'), $schedulesdata[0]->name);
+        $this->assertEquals($schedule->get('subject'), $schedulesdata[0]->subject);
+        $this->assertEquals($schedule->get('message'), $schedulesdata[0]->message);
+        $this->assertEquals($schedule->get('enabled'), $schedulesdata[0]->enabled);
+        $this->assertEquals($schedule->get('audiences'), $schedulesdata[0]->audiences);
+        $this->assertNotEmpty($schedulesdata[0]->timescheduled);
+        $this->assertNotEmpty($schedulesdata[0]->timelastsent);
+        $this->assertEquals($schedule->get('recurrence'), $schedulesdata[0]->recurrence);
+        $this->assertNotEmpty($schedulesdata[0]->timenextsend);
+        $this->assertEquals($schedule->get('userviewas'), $schedulesdata[0]->userviewas);
+        $this->assertEquals($user->id, $schedulesdata[0]->usercreated);
+        $this->assertEquals($user->id, $schedulesdata[0]->usermodified);
+        $this->assertNotEmpty($schedulesdata[0]->timecreated);
+        $this->assertNotEmpty($schedulesdata[0]->timemodified);
+    }
+
+    /**
+     * Test class test_export_user_data method with a different user
+     */
+    public function test_export_user_data_different_user(): void {
+        $this->resetAfterTest();
+
+        /** @var core_reportbuilder_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('core_reportbuilder');
+
+        $user1 = $this->getDataGenerator()->create_user();
+        $this->setUser($user1);
+
+        $report1 = $generator->create_report([
+            'name' => 'My report',
+            'type' => base::TYPE_CUSTOM_REPORT,
+            'source' => users::class,
+        ]);
+        $audience1a = $generator->create_audience([
+            'reportid' => $report1->get('id'),
+            'configdata' => ['users' => [$user1->id]],
+        ]);
+
+        // Add a second user who creates and audience in $report and creates a new $report2 with another audience.
+        $user2 = $this->getDataGenerator()->create_user();
+        $this->setUser($user2);
+
+        $report2 = $generator->create_report([
+            'name' => 'My report 2',
+            'type' => base::TYPE_CUSTOM_REPORT,
+            'source' => users::class,
+        ]);
+        $audience2 = $generator->create_audience([
+            'reportid' => $report2->get('id'),
+            'configdata' => ['users' => [$user2->id]],
+        ]);
+        $audience1b = $generator->create_audience([
+            'reportid' => $report1->get('id'),
+            'configdata' => ['users' => [$user2->id]],
+        ]);
+
+        $this->setUser(null);
+
+        $context = context_system::instance();
+        $this->export_context_data_for_user((int)$user2->id, $context, 'core_reportbuilder');
+
+        $writer = writer::with_context($context);
+
+        // Get report1 exported data.
+        $contextpath = provider::get_export_path($report1);
+
+        $reportdata = $writer->get_data($contextpath);
+        $this->assertEquals($report1->get('name'), $reportdata->name);
+        $this->assertEquals($report1->get('source'), $reportdata->source);
+        $this->assertEquals($user1->id, $reportdata->usercreated);
+        $this->assertEquals($user1->id, $reportdata->usermodified);
+        $this->assertNotEmpty($reportdata->timecreated);
+        $this->assertNotEmpty($reportdata->timemodified);
+
+        // Get audience exported data.
+        $audiencesdata = $writer->get_related_data($contextpath, 'audiences')->data;
+        $this->assertCount(1, $audiencesdata);
+        $this->assertEquals($audience1b->get_persistent()->get('classname'), $audiencesdata[0]->classname);
+        $this->assertEquals($audience1b->get_persistent()->get('configdata'), $audiencesdata[0]->configdata);
+        $this->assertEquals($user2->id, $audiencesdata[0]->usercreated);
+        $this->assertEquals($user2->id, $audiencesdata[0]->usermodified);
+        $this->assertNotEmpty($audiencesdata[0]->timecreated);
+        $this->assertNotEmpty($audiencesdata[0]->timemodified);
+
+        // Get report2 exported data.
+        $contextpath = provider::get_export_path($report2);
+
+        $reportdata2 = $writer->get_data($contextpath);
+        $this->assertEquals($report2->get('name'), $reportdata2->name);
+        $this->assertEquals($report2->get('source'), $reportdata2->source);
+        $this->assertEquals($user2->id, $reportdata2->usercreated);
+        $this->assertEquals($user2->id, $reportdata2->usermodified);
+        $this->assertNotEmpty($reportdata2->timecreated);
+        $this->assertNotEmpty($reportdata2->timemodified);
+
+        // Get audience exported data.
+        $audiencesdata2 = $writer->get_related_data($contextpath, 'audiences')->data;
+        $this->assertCount(1, $audiencesdata2);
+        $this->assertEquals($audience2->get_persistent()->get('classname'), $audiencesdata2[0]->classname);
+        $this->assertEquals($audience2->get_persistent()->get('configdata'), $audiencesdata2[0]->configdata);
+        $this->assertEquals($user2->id, $audiencesdata2[0]->usercreated);
+        $this->assertEquals($user2->id, $audiencesdata2[0]->usermodified);
+        $this->assertNotEmpty($audiencesdata2[0]->timecreated);
+        $this->assertNotEmpty($audiencesdata2[0]->timemodified);
     }
 }
